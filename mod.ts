870e339e173472187318c74f81d0dae4e74e7fa4
mineejo <mineejo.dev@gmail.com>
2023-11-11 15:55:16 +0300

    chore(mod.ts): update doc module

--- mod.ts
+++ mod.ts
@@ -1,5 +1,11 @@
 // Copyright 2023 mineejo. All rights reserved. MIT license.
 
+/**
+ * Color is a tool for creating RGB color by range and by HSL without leaving RGB space. With additional tools, unlimited development and no dependencies.
+ *
+ * @module
+ */
+
 export { Color } from "./color.ts";
 export { AdvancedColor } from "./advanced_color.ts";
 export { ColorComponent } from "./color_component.ts";
