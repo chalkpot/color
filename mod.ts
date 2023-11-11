c89d1785698ff2ab48b9c08f73e36ed9aa76ff7b
mineejo <mineejo.dev@gmail.com>
2023-11-11 15:57:29 +0300

    chore(mod.ts): format comment

--- mod.ts
+++ mod.ts
@@ -1,7 +1,9 @@
 // Copyright 2023 mineejo. All rights reserved. MIT license.
 
 /**
- * Color is a tool for creating RGB color by range and by HSL without leaving RGB space. With additional tools, unlimited development and no dependencies.
+ * Color is a tool for creating RGB color by range and by HSL
+ * without leaving RGB space. With additional tools,
+ * unlimited development and no dependencies.
  *
  * @module
  */
