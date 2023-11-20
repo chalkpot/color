d2275367537fefbf4742abec99e973d0a50c0a5f
mineejo <mineejo.dev@gmail.com>
2023-11-20 15:09:13 +0300

    feat: normalize type exports

--- color/mod.ts
+++ color/mod.ts
@@ -1,4 +1,6 @@
 // Copyright 2023 mineejo. All rights reserved. MIT license.
 
 export { rgbColorWheel } from "./rgb_color_wheel.ts";
+
 export type { Rgb } from "./rgb.ts";
+export type { Rgba } from "./rgba.ts";
--- mod.ts
+++ mod.ts
@@ -13,3 +13,5 @@ export { AdvancedColor } from "./advanced_color.ts";
 export { ColorComponent } from "./color_component.ts";
 
 export * from "./color_effects/mod.ts";
+
+export type * from "./color/mod.ts";
